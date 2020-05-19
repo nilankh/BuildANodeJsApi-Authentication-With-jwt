4ea5afca750c9a8371809a78a5c99af3b9a33c00
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 17:23:36 +0530

    create config file mongoose js where i have written code for connectiing to database

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+//ye database se connect krega
+mongoose.connect('mongodb://localhost/learing_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'error connecting to db'));
+// finally if connection is successful
+db.once('open', function() {
+    // we are connected !
+    console.log('Successfuuly connected to database');
+});
+module.exports = db;
