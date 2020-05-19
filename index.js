3fdd7ea41ea4dbe16f3cfa257e4121dc3901fd28
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 17:21:26 +0530

    created routes and several folder which is required

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// Import ROutes
+const authRoute = require('./routes/auth');
+
+// Routes middleware
+app.use('/api/user', authRoute);
+
+
 // Listen for requests
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server, ${err}`)};
