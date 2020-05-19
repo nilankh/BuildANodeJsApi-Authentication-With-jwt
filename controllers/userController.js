30e8969b413b0bb62cc3252f11f4143b71583c8b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 21:07:55 +0530

    installed joi and set up some authentication part like email should be valid password length should be minimum 6 name should be minimum like this

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,36 @@ const mongoose = require('mongoose');
 
 const User = require('../models/User');
 
+// Validation
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    name: Joi.string()
+        .min(6)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(6)
+        .required()
+});
+
+
 // For registring
 module.exports.registerUser = async function(req, res){
+    
+    // Lets validate the data before we a user
+    // const validation = schema.validate(req.body);
+    // res.send(validation);ye v sahi h par hme sirfd basic data chahiye error ka
 
+    // ye do line bs error msg show krega or upar wala pura details dega
+    const { error } = schema.validate(req.body);
+    // res.send(error.details[0].message);
+    if(error){
+        return res.status(400).send(error.details[0].message);
+
+    }
     // Creation of data base
     const user = new User({
         name: req.body.name,
